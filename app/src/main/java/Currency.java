e9637c620744223d9a6e336b00e020b9977ec573
amirhome <amir.hosseinzadeh@flo.com.tr>
2019-08-13 19:30:18 +0300

    Retrieve currency and cost and set image of main section

---
+++ app/src/main/java/Currency.java
@@ -0,0 +1,10 @@
+public class Currency {
+    public String author;
+    public String title;
+
+    public Currency(String author, String title) {
+//        this.author = author;
+//        this.title = title;
+    }
+}
+
--- app/src/main/java/com/example/droidforex/MainActivity.java
+++ app/src/main/java/com/example/droidforex/MainActivity.java
@@ -4,10 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.progress.progressview.ProgressView;
 
+import java.util.Currency;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AmirHome";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +34,61 @@ public class MainActivity extends AppCompatActivity {
         ProgressView rProgressView = findViewById(R.id.progressView2);
         rProgressView.applyGradient(colorList);
 
+        lProgressView.setProgress((float) 0.7);
+        rProgressView.setProgress(1);
+
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("forex/latest");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    TextView textViewCost;
+                    TextView textViewCurrency;
+                    ImageView imageView;
+                    switch (ds.getKey()) {
+                        case "EUR_TRY":
+                            textViewCurrency = findViewById(R.id.textView1);
+                            textViewCurrency.setText(ds.getKey());
+                            textViewCost = findViewById(R.id.textView2);
+                            textViewCost.setText(ds.child("cost").getValue().toString());
+                            imageView = findViewById(R.id.imageView1);
+                            imageView.setImageResource(R.mipmap.fl_ic_eur_tr);
+                            break;
+                        case "USD_EUR":
+                            textViewCurrency = findViewById(R.id.textView3);
+                            textViewCurrency.setText(ds.getKey());
+                            textViewCost= findViewById(R.id.textView4);
+                            textViewCost.setText(ds.child("cost").getValue().toString());
+                            imageView = findViewById(R.id.imageView2);
+                            imageView.setImageResource(R.mipmap.fl_ic_us_eur);
+                            break;
+                        case "USD_TRY":
+                            textViewCurrency = findViewById(R.id.textView5);
+                            textViewCurrency.setText(ds.getKey());
+                            textViewCost = findViewById(R.id.textView6);
+                            textViewCost.setText(ds.child("cost").getValue().toString());
+                            imageView = findViewById(R.id.imageView3);
+                            imageView.setImageResource(R.mipmap.fl_ic_us_tr);
+                            break;
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
+
 }
